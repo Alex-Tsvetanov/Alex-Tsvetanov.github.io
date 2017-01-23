47bad93a82eadfc3e28dfef8a9db34d28404b0e2
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-01-23 21:45:46 +0200

    New

--- NOI/I/Solutions/D/TMD/numbers.cpp
+++ NOI/I/Solutions/D/TMD/numbers.cpp
@@ -2,17 +2,17 @@
 #include<cmath>
 using namespace std;
 int main(){
-long long m,n,i,a,br=0;
-cin>>m>>n;
-for(i=m;i<=n;i++){
-a=sqrt(i);
-if(a*(a+1)==i){
-break;
-}
-}
-for(i=a;i*(i+1)<=n;i++){
-br++;
-}
-cout<<br<<endl;
-return 0;
+	long long m,n,i,a,br=0;
+	cin>>m>>n;
+	for(i=m;i<=n;i++){
+		a=sqrt(i);
+		if(a*(a+1)==i){
+			break;
+		}
+	}
+	for(i=a;i*(i+1)<=n;i++){
+		br++;
+	}
+	cout<<br<<endl;
+	return 0;
 }
